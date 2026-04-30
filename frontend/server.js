fbf20b3bd00592edc6da8f1c92e4ddc6c8f8617e
EDITH-KRP <developer@example.com>
2026-05-01 00:43:55 +0530

    Fix Express 5 wildcard routing error

--- frontend/server.js
+++ frontend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
